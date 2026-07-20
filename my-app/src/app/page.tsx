3b5b3a55e4067373a64e646abea534918032ac57
IsaacSousa2 <ciceroisaacsc@gmail.com>
2026-07-20 07:55:29 -0300

    fix: commum component css

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,22 @@
 import HeroSection from "@/components/HeroSection";
 import Header from "../components/Header";
-
+import ComumComponent from "@/components/ComumComponent";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <ComumComponent 
+        comment="O problema"
+        tittle="Porque investir parece tão difícil?"
+        subtittle="Muita informação? Escolhas dificeis?"
+        text="No fim, muitas pessoas deixam de investir não por falta de interesse, mas porque a quantidade de informações, opiniões e gráficos complexos torna a tomada de decisão confusa e cansativa."
+        image={{
+          src: "/box-differentials.svg",
+          alt: "Diferenciais fluxo"
+        }}
+      />
     </div>
   );
 }
